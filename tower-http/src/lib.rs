43043237fd22dbe558f18650a48f7025fb5964d4
Carl Lerche <me@carllerche.com>
2019-05-30 11:38:31 -0700

    Remove re-exports for now (#26)

--- tower-http/src/lib.rs
+++ tower-http/src/lib.rs
@@ -1,2 +1,4 @@
 pub use http_body::Body;
-pub use tower_http_util::{BodyExt, HttpMakeConnection, HttpService};
+pub use tower_http_util::body::BodyExt;
+pub use tower_http_util::connection::HttpMakeConnection;
+pub use tower_http_util::service::HttpService;
