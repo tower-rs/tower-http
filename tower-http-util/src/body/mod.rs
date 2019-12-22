b71e6c3033dae1382f643d59174aaf668e236009
Daiki Mizukami <tesaguriguma@gmail.com>
2019-12-23 00:29:38 +0900

    Remove `BodyExt::into_buf_stream`

--- tower-http-util/src/body/mod.rs
+++ tower-http-util/src/body/mod.rs
@@ -1,20 +1,8 @@
 //! Types and utilities for working with `Body`.
 
-mod into_buf_stream;
-
-pub use self::into_buf_stream::IntoBufStream;
-
 use http_body::Body;
 
 /// An extension trait for `Body` providing additional adapters.
-pub trait BodyExt: Body {
-    /// Wrap the `Body` so that it implements tokio_buf::BufStream directly.
-    fn into_buf_stream(self) -> IntoBufStream<Self>
-    where
-        Self: Sized,
-    {
-        IntoBufStream::new(self)
-    }
-}
+pub trait BodyExt: Body {}
 
 impl<T: Body> BodyExt for T {}
