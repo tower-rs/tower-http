9ca9e7cc56a43608808688a7968f21198cb0ed20
David Pedersen <david.pdrsn@gmail.com>
2021-01-26 11:07:07 +0100

    Remove all code. We are starting over (#32)

--- tower-http-util/src/body/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Types and utilities for working with `Body`.
-
-use http_body::Body;
-
-/// An extension trait for `Body` providing additional adapters.
-pub trait BodyExt: Body {}
-
-impl<T: Body> BodyExt for T {}
--- tower-http/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub use http_body::Body;
-pub use tower_http_util::body::BodyExt;
-pub use tower_http_util::connection::HttpMakeConnection;
-pub use tower_http_util::service::HttpService;
