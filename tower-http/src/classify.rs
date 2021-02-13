2b99d8c819b495ddc3ee16268cfa66d36eb8becc
David Pedersen <david.pdrsn@gmail.com>
2021-02-13 18:05:03 +0100

    More API changes based on review feedback

--- tower-http/src/classify.rs
+++ tower-http/src/classify.rs
@@ -1,56 +1,86 @@
 //! Tools for classifying responses as either success or failure.
 
-use http::{HeaderMap, Response, StatusCode};
+use http::{HeaderMap, Request, Response, StatusCode};
 use http_body::Body;
 use std::{convert::Infallible, marker::PhantomData};
 
-/// Trait for classifying responses as either success or failure.
-pub trait ClassifyResponse {
-    /// The output of doing the classification.
-    ///
-    /// This could be an HTTP status code or some other kind of status.
-    type Output;
+/// Trait for producing response classifiers from a request.
+///
+/// This is useful if your classifier depends on data from the request. Could for example be the
+/// URI or HTTP method.
+pub trait MakeClassifier {
+    /// The response classifier produced.
+    type Classifier: ClassifyResponse<
+        FailureClass = Self::FailureClass,
+        ClassifyEos = Self::ClassifyEos,
+    >;
+
+    /// The type of failure classifications.
+    type FailureClass;
 
     /// The type used to classify the response end of stream (EOS).
-    type ClassifyEos: ClassifyEos<Output = Self::Output>;
+    type ClassifyEos: ClassifyEos<FailureClass = Self::FailureClass>;
 
-    /// Perform the classification.
-    fn classify_response<B>(
-        &mut self,
-        res: &Response<B>,
-    ) -> ClassifiedNowOrLater<Self::Output, Self::ClassifyEos>
+    /// Make a response classifier for this request
+    fn make_classify<B>(&self, req: &Request<B>) -> Self::Classifier
     where
         B: Body;
 }
 
+/// A `MakeClassifier` that works by cloning a classifier.
+#[derive(Debug, Clone)]
+pub struct SharedClassifier<C> {
+    classifier: C,
+}
+
+impl<C> SharedClassifier<C> {
+    pub fn new(classifier: C) -> Self
+    where
+        C: ClassifyResponse + Clone,
+    {
+        Self { classifier }
+    }
+}
+
+/// Trait for classifying responses as either success or failure.
+pub trait ClassifyResponse {
+    /// The type of failure classifications.
+    type FailureClass;
+
+    /// The type used to classify the response end of stream (EOS).
+    type ClassifyEos: ClassifyEos<FailureClass = Self::FailureClass>;
+
+    /// Classify a response.
+    fn classify_response<B>(
+        self,
+        res: &Response<B>,
+    ) -> ClassifiedNowOrLater<Self::FailureClass, Self::ClassifyEos>
+    where
+        B: Body;
+
+    /// Classify an error
+    fn classify_error<E>(self, error: &E) -> Self::FailureClass
+    where
+        E: std::error::Error + 'static;
+}
+
 /// Trait for classifying end of streams (EOS) as either success or failure.
 pub trait ClassifyEos {
-    /// The output of doing the classification.
-    ///
-    /// This could be an HTTP status code or some other kind of status.
-    type Output;
+    /// The type of failure classifications.
+    type FailureClass;
 
     /// Perform the classification.
-    fn classify_eos(&mut self, trailers: Option<&HeaderMap>) -> Classification<Self::Output>;
+    fn classify_eos(self, trailers: Option<&HeaderMap>) -> Result<(), Self::FailureClass>;
 }
 
 /// Result of doing a classification.
 #[derive(Debug)]
 // really don't like this name... :(
-pub enum ClassifiedNowOrLater<T, ClassifyEos> {
+pub enum ClassifiedNowOrLater<FailureClass, ClassifyEos> {
     /// The response was able to be classified immediately.
-    Ready(Classification<T>),
+    Ready(Result<(), FailureClass>),
     /// We have to wait until the end of a streaming response to classify it.
     RequiresEos(ClassifyEos),
-}
-
-/// A classification of a response.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Classification<T> {
-    /// The response was a success.
-    Success(T),
-    /// The response was a failure.
-    Failure(T),
 }
 
 /// A [`ClassifyEos`] type that can be used in [`ClassifyResponse`] implementations that never have
@@ -58,16 +88,32 @@ pub enum Classification<T> {
 ///
 /// `NeverClassifyEos` exists only as type. You cannot construct `NeverClassifyEos` values.
 pub struct NeverClassifyEos<T> {
-    _output_ty: PhantomData<T>,
+    _output_ty: PhantomData<fn() -> T>,
     _never: Infallible,
 }
 
 impl<T> ClassifyEos for NeverClassifyEos<T> {
-    type Output = T;
+    type FailureClass = T;
 
-    fn classify_eos(&mut self, _trailers: Option<&HeaderMap>) -> Classification<Self::Output> {
+    fn classify_eos(self, _trailers: Option<&HeaderMap>) -> Result<(), Self::FailureClass> {
         // `NeverClassifyEos` contains an `Infallible` so it can never be constructed
         unreachable!()
+    }
+}
+
+impl<C> MakeClassifier for SharedClassifier<C>
+where
+    C: ClassifyResponse + Clone,
+{
+    type FailureClass = C::FailureClass;
+    type ClassifyEos = C::ClassifyEos;
+    type Classifier = C;
+
+    fn make_classify<B>(&self, _req: &Request<B>) -> Self::Classifier
+    where
+        B: Body,
+    {
+        self.classifier.clone()
     }
 }
 
@@ -87,21 +133,28 @@ impl ServerErrorsAsFailures {
 }
 
 impl ClassifyResponse for ServerErrorsAsFailures {
-    type Output = http::StatusCode;
+    type FailureClass = StatusCode;
     type ClassifyEos = NeverClassifyEos<StatusCode>;
 
     fn classify_response<B>(
-        &mut self,
+        self,
         res: &Response<B>,
-    ) -> ClassifiedNowOrLater<Self::Output, Self::ClassifyEos>
+    ) -> ClassifiedNowOrLater<Self::FailureClass, Self::ClassifyEos>
     where
         B: Body,
     {
         if res.status().is_server_error() {
-            ClassifiedNowOrLater::Ready(Classification::Failure(res.status()))
+            ClassifiedNowOrLater::Ready(Err(res.status()))
         } else {
-            ClassifiedNowOrLater::Ready(Classification::Success(res.status()))
+            ClassifiedNowOrLater::Ready(Ok(()))
         }
+    }
+
+    fn classify_error<E>(self, _error: &E) -> Self::FailureClass
+    where
+        E: std::error::Error,
+    {
+        StatusCode::INTERNAL_SERVER_ERROR
     }
 }
 
@@ -124,13 +177,13 @@ impl GrpcErrorsAsFailures {
 }
 
 impl ClassifyResponse for GrpcErrorsAsFailures {
-    type Output = i32;
+    type FailureClass = i32;
     type ClassifyEos = GrpcEosErrorsAsFailures;
 
     fn classify_response<B>(
-        &mut self,
+        self,
         res: &Response<B>,
-    ) -> ClassifiedNowOrLater<Self::Output, Self::ClassifyEos>
+    ) -> ClassifiedNowOrLater<Self::FailureClass, Self::ClassifyEos>
     where
         B: Body,
     {
@@ -139,6 +192,13 @@ impl ClassifyResponse for GrpcErrorsAsFailures {
         } else {
             ClassifiedNowOrLater::RequiresEos(GrpcEosErrorsAsFailures { _priv: () })
         }
+    }
+
+    fn classify_error<E>(self, _error: &E) -> Self::FailureClass
+    where
+        E: std::error::Error + 'static,
+    {
+        13
     }
 }
 
@@ -149,22 +209,21 @@ pub struct GrpcEosErrorsAsFailures {
 }
 
 impl ClassifyEos for GrpcEosErrorsAsFailures {
-    type Output = i32;
+    type FailureClass = i32;
 
-    fn classify_eos(&mut self, trailers: Option<&HeaderMap>) -> Classification<Self::Output> {
-        classify_grpc_metadata(trailers.expect("no trailers"))
-            .expect("no or invalid grpc-status in trailers")
+    fn classify_eos(self, trailers: Option<&HeaderMap>) -> Result<(), i32> {
+        trailers.and_then(classify_grpc_metadata).unwrap_or(Ok(()))
     }
 }
 
-fn classify_grpc_metadata(headers: &HeaderMap) -> Option<Classification<i32>> {
+fn classify_grpc_metadata(headers: &HeaderMap) -> Option<Result<(), i32>> {
     let status = headers.get("grpc-status")?;
     let status = status.to_str().ok()?;
     let status = status.parse::<i32>().ok()?;
 
     if status == 0 {
-        Some(Classification::Success(status))
+        Some(Ok(()))
     } else {
-        Some(Classification::Failure(status))
+        Some(Err(status))
     }
 }
--- tower-http/src/trace.rs
+++ tower-http/src/trace.rs
@@ -4,7 +4,8 @@
 //! [`Service`]: tower_service::Service
 
 use crate::classify::{
-    ClassifiedNowOrLater, ClassifyEos, ClassifyResponse, ServerErrorsAsFailures,
+    ClassifiedNowOrLater, ClassifyEos, ClassifyResponse, GrpcErrorsAsFailures, MakeClassifier,
+    ServerErrorsAsFailures, SharedClassifier,
 };
 use http::{HeaderMap, Request, Response};
 use http_body::Body;
@@ -13,7 +14,6 @@ use std::{
     future::Future,
     pin::Pin,
     task::{Context, Poll},
-    time::Instant,
 };
 use tower_layer::Layer;
 use tower_service::Service;
@@ -24,26 +24,27 @@ use tower_service::Service;
 /// [tracing]: https://crates.io/crates/tracing
 /// [`Service`]: tower_service::Service
 #[derive(Clone, Default)]
-pub struct TraceLayer<C = ServerErrorsAsFailures> {
-    classifier: C,
+pub struct TraceLayer<C> {
+    make_classifier: C,
 }
 
-impl TraceLayer<ServerErrorsAsFailures> {
-    /// Create a new [`TraceLayer`] using [`ServerErrorsAsFailures`] as the response classifier.
+impl TraceLayer<SharedClassifier<ServerErrorsAsFailures>> {
+    /// Create a new [`TraceLayer`] using [`ServerErrorsAsFailures`] which supports classifying
+    /// regular HTTP responses based on the status code.
     pub fn new() -> Self {
         Self {
-            classifier: ServerErrorsAsFailures::new(),
+            make_classifier: SharedClassifier::new(ServerErrorsAsFailures::default()),
         }
     }
 }
 
-impl<C> TraceLayer<C> {
-    /// Provider another response classifier to use.
-    pub fn with_classifier<NewClassifier>(
-        self,
-        classifier: NewClassifier,
-    ) -> TraceLayer<NewClassifier> {
-        TraceLayer { classifier }
+impl TraceLayer<SharedClassifier<GrpcErrorsAsFailures>> {
+    /// Create a new [`TraceLayer`] using [`GrpcErrorsAsFailures`] which supports classifying
+    /// gRPC responses and streams based on the `grpc-status` header.
+    pub fn new_for_grpc() -> Self {
+        Self {
+            make_classifier: SharedClassifier::new(GrpcErrorsAsFailures::default()),
+        }
     }
 }
 
@@ -56,7 +57,7 @@ where
     fn layer(&self, inner: S) -> Self::Service {
         Trace {
             inner,
-            classifier: self.classifier.clone(),
+            make_classifier: self.make_classifier.clone(),
         }
     }
 }
@@ -67,32 +68,31 @@ where
 /// [`Service`]: tower_service::Service
 pub struct Trace<S, C> {
     inner: S,
-    classifier: C,
+    make_classifier: C,
 }
 
 impl<S, ReqBody, ResBody, C> Service<Request<ReqBody>> for Trace<S, C>
 where
     S: Service<Request<ReqBody>, Response = Response<ResBody>>,
+    ReqBody: Body,
     ResBody: Body,
-    C: ClassifyResponse + Clone,
+    C: MakeClassifier,
+    C::Classifier: Clone,
 {
     type Response = Response<TraceBody<ResBody, C::ClassifyEos>>;
     type Error = S::Error;
-    type Future = ResponseFuture<S::Future, C>;
+    type Future = ResponseFuture<S::Future, C::Classifier>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
     }
 
     fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
-        let start = Instant::now();
-
-        // TODO(david): log that we received a request
+        let classifier = self.make_classifier.make_classify(&req);
 
         ResponseFuture {
             inner: self.inner.call(req),
-            start,
-            classifier: self.classifier.clone(),
+            classifier: Some(classifier),
         }
     }
 }
@@ -102,8 +102,7 @@ where
 pub struct ResponseFuture<F, C> {
     #[pin]
     inner: F,
-    start: Instant,
-    classifier: C,
+    classifier: Option<C>,
 }
 
 impl<F, ResBody, E, C> Future for ResponseFuture<F, C>
@@ -117,18 +116,13 @@ where
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let this = self.project();
         let result = futures_util::ready!(this.inner.poll(cx));
-        let latency = this.start.elapsed();
-        let start = *this.start;
 
         match result {
-            Ok(res) => match this.classifier.classify_response(&res) {
+            Ok(res) => match this.classifier.take().unwrap().classify_response(&res) {
                 ClassifiedNowOrLater::Ready(classification) => {
-                    // TODO(david): use classification and latency
-
                     let res = res.map(|body| TraceBody {
                         inner: body,
                         classify_eos: None,
-                        start,
                     });
                     Poll::Ready(Ok(res))
                 }
@@ -136,7 +130,6 @@ where
                     let res = res.map(|body| TraceBody {
                         inner: body,
                         classify_eos: Some(classify_eos),
-                        start,
                     });
                     Poll::Ready(Ok(res))
                 }
@@ -155,7 +148,6 @@ pub struct TraceBody<B, C> {
     #[pin]
     inner: B,
     classify_eos: Option<C>,
-    start: Instant,
 }
 
 impl<B, C> Body for TraceBody<B, C>
@@ -180,11 +172,30 @@ where
         let this = self.project();
         let trailers = futures_util::ready!(this.inner.poll_trailers(cx)?);
 
-        if let Some(mut classify_eos) = this.classify_eos.take() {
+        if let Some(classify_eos) = this.classify_eos.take() {
             let classification = classify_eos.classify_eos(trailers.as_ref());
-            // TODO(david): use classification and start
         }
 
         Poll::Ready(Ok(trailers))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    use hyper::{Body, Error, Request, Response};
+    use tower::{service_fn, ServiceBuilder};
+
+    #[allow(warnings)]
+    fn what_does_the_api_feel_like() {
+        let _svc = ServiceBuilder::new()
+            .layer(TraceLayer::new())
+            .service(service_fn(handle));
+    }
+
+    #[allow(warnings)]
+    async fn handle(_req: Request<Body>) -> Result<Response<Body>, Error> {
+        todo!()
     }
 }
