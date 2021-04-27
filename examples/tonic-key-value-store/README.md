2c63532508594c06f78f3b845143b0ef1f7ff746
David Pedersen <david.pdrsn@gmail.com>
2021-04-27 20:48:56 +0200

    Disable compression for gRPC and add tonic streaming example (#82)
    * Disable compression for gRPC
    
    * Don't add `accept-encoding` to grpc requests
    
    * Remove debugging code
    
    * Disable formatting of tonic build output
    
    Because it breaks CI
    
    * Apply suggestions from code review
    
    Co-authored-by: Lucio Franco <luciofranco14@gmail.com>
    
    * Address review feedback
    
    Co-authored-by: Lucio Franco <luciofranco14@gmail.com>

--- examples/tonic-key-value-store/README.md
+++ examples/tonic-key-value-store/README.md
@@ -7,7 +7,8 @@ This examples contains a simple key/value store with a gRPC API and client built
 Running a server:
 
 ```
-RUST_LOG=tower_http=trace cargo run --bin tonic-key-value-store -- -p 3000 server
+RUST_LOG=tonic_key_value_store=trace,tower_http=trace \
+    cargo run --bin tonic-key-value-store -- -p 3000 server
 ```
 
 Setting values:
@@ -20,4 +21,10 @@ Getting values:
 
 ```
 RUST_LOG=tower_http=trace cargo run --bin tonic-key-value-store -- -p 3000 get -k foo
+```
+
+Create a stream of new keys:
+
+```
+cargo run --bin tonic-key-value-store -- -p 3000 subscribe
 ```
--- tower-http/src/compression/future.rs
+++ tower-http/src/compression/future.rs
@@ -1,9 +1,9 @@
 #![allow(unused_imports)]
 
 use super::{body::BodyInner, CompressionBody, Encoding};
-use crate::compression_utils::{BoxError, WrapBody};
+use crate::compression_utils::{supports_transparent_compression, BoxError, WrapBody};
 use futures_util::ready;
-use http::{header, HeaderValue, Response};
+use http::{header, HeaderMap, HeaderValue, Response};
 use http_body::Body;
 use pin_project::pin_project;
 use std::{
@@ -36,19 +36,24 @@ where
 
         let (mut parts, body) = res.into_parts();
 
-        let body = match self.encoding {
-            #[cfg(feature = "compression-gzip")]
-            Encoding::Gzip => CompressionBody(BodyInner::Gzip(WrapBody::new(body))),
-            #[cfg(feature = "compression-deflate")]
-            Encoding::Deflate => CompressionBody(BodyInner::Deflate(WrapBody::new(body))),
-            #[cfg(feature = "compression-br")]
-            Encoding::Brotli => CompressionBody(BodyInner::Brotli(WrapBody::new(body))),
-            Encoding::Identity => {
+        let body = match (
+            supports_transparent_compression(&parts.headers),
+            self.encoding,
+        ) {
+            // if compression is _not_ support or the client doesn't accept it
+            (false, _) | (_, Encoding::Identity) => {
                 return Poll::Ready(Ok(Response::from_parts(
                     parts,
                     CompressionBody(BodyInner::Identity(body)),
                 )))
             }
+
+            #[cfg(feature = "compression-gzip")]
+            (_, Encoding::Gzip) => CompressionBody(BodyInner::Gzip(WrapBody::new(body))),
+            #[cfg(feature = "compression-deflate")]
+            (_, Encoding::Deflate) => CompressionBody(BodyInner::Deflate(WrapBody::new(body))),
+            #[cfg(feature = "compression-br")]
+            (_, Encoding::Brotli) => CompressionBody(BodyInner::Brotli(WrapBody::new(body))),
         };
 
         parts.headers.remove(header::CONTENT_LENGTH);
