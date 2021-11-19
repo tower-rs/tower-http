d3e9d3fd28ad360824ec24e5da315fe76a63cf7a
David Pedersen <david.pdrsn@gmail.com>
2021-11-19 12:32:55 +0100

    change things around a bit

--- tower-http/src/request_id.rs
+++ tower-http/src/request_id.rs
@@ -1,63 +1,28 @@
+// NOTE: when uuid 1.0 is shipped we can include a `MakeRequestId` that uses that
+// See https://github.com/uuid-rs/uuid/issues/113
+
 use http::{
     header::{HeaderName, HeaderValue},
-    request::Parts,
-    Request,
+    Request, Response,
 };
+use pin_project_lite::pin_project;
 use std::task::{Context, Poll};
+use std::{future::Future, pin::Pin};
 use tower_layer::Layer;
 use tower_service::Service;
 
+pub(crate) const X_REQUEST_ID: &str = "x-request-id";
+
 pub trait MakeRequestId {
-    fn make_request_id(&mut self, request_parts: &Parts) -> (HeaderName, Option<RequestId>);
-}
-
-impl<F> MakeRequestId for F
-where
-    F: FnMut(&Parts) -> (HeaderName, Option<RequestId>),
-{
-    fn make_request_id(&mut self, request_parts: &Parts) -> (HeaderName, Option<RequestId>) {
-        self(request_parts)
-    }
-}
-
-#[cfg(feature = "uuid")]
-#[cfg_attr(docsrs, doc(cfg(feature = "uuid")))]
-#[derive(Debug, Clone)]
-pub struct UuidRequestId {
-    header_name: HeaderName,
-}
-
-#[cfg(feature = "uuid")]
-#[cfg_attr(docsrs, doc(cfg(feature = "uuid")))]
-impl UuidRequestId {
-    pub fn new(header_name: HeaderName) -> Self {
-        Self { header_name }
-    }
-}
-
-#[cfg(feature = "uuid")]
-impl MakeRequestId for UuidRequestId {
-    fn make_request_id(&mut self, request_parts: &Parts) -> (HeaderName, Option<RequestId>) {
-        if request_parts.headers.contains_key(&self.header_name) {
-            (self.header_name.clone(), None)
-        } else {
-            let id = HeaderValue::from_str(&uuid::Uuid::new_v4().to_string())
-                .expect("uuid wasn't valid header value");
-            let id = RequestId::new(id);
-            (self.header_name.clone(), Some(id))
-        }
-    }
+    fn make_request_id<B>(&mut self, request: &Request<B>) -> RequestId;
 }
 
 #[derive(Debug, Clone)]
 pub struct RequestId(HeaderValue);
 
 impl RequestId {
-    pub fn new<T>(header_value: T) -> Self
-    where
-        T: Into<HeaderValue>,
-    {
-        Self(header_value.into())
+    pub fn new(header_value: HeaderValue) -> Self {
+        Self(header_value)
     }
 
     pub fn header_value(&self) -> &HeaderValue {
@@ -67,28 +32,27 @@ impl RequestId {
     pub fn into_header_value(self) -> HeaderValue {
         self.0
     }
-
-    pub fn from_request<B>(request: &Request<B>) -> Option<Self> {
-        request.extensions().get().cloned()
-    }
 }
 
 #[derive(Debug, Clone)]
 pub struct SetRequestIdLayer<M> {
+    header_name: HeaderName,
     make_request_id: M,
 }
 
 impl<M> SetRequestIdLayer<M> {
-    pub fn new(make_request_id: M) -> Self {
-        SetRequestIdLayer { make_request_id }
+    pub fn new(header_name: HeaderName, make_request_id: M) -> Self {
+        SetRequestIdLayer {
+            header_name,
+            make_request_id,
+        }
     }
-}
 
-#[cfg(feature = "uuid")]
-#[cfg_attr(docsrs, doc(cfg(feature = "uuid")))]
-impl SetRequestIdLayer<UuidRequestId> {
-    pub fn uuid(header_name: HeaderName) -> Self {
-        Self::new(UuidRequestId::new(header_name))
+    pub fn x_request_id(make_request_id: M) -> Self {
+        SetRequestIdLayer {
+            header_name: HeaderName::from_static(X_REQUEST_ID),
+            make_request_id,
+        }
     }
 }
 
@@ -99,42 +63,48 @@ where
     type Service = SetRequestId<S, M>;
 
     fn layer(&self, inner: S) -> Self::Service {
-        SetRequestId::new(inner, self.make_request_id.clone())
+        SetRequestId::new(
+            inner,
+            self.header_name.clone(),
+            self.make_request_id.clone(),
+        )
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct SetRequestId<S, M> {
     inner: S,
+    header_name: HeaderName,
     make_request_id: M,
 }
 
 impl<S, M> SetRequestId<S, M> {
-    pub fn new(inner: S, make_request_id: M) -> Self {
+    pub fn new(inner: S, header_name: HeaderName, make_request_id: M) -> Self {
         Self {
             inner,
+            header_name,
             make_request_id,
         }
     }
 
+    pub fn x_request_id(inner: S, make_request_id: M) -> Self {
+        Self::new(
+            inner,
+            HeaderName::from_static(X_REQUEST_ID),
+            make_request_id,
+        )
+    }
+
     define_inner_service_accessors!();
 
-    pub fn layer(make_request_id: M) -> SetRequestIdLayer<M> {
-        SetRequestIdLayer::new(make_request_id)
+    pub fn layer(header_name: HeaderName, make_request_id: M) -> SetRequestIdLayer<M> {
+        SetRequestIdLayer::new(header_name, make_request_id)
     }
 }
 
-#[cfg(feature = "uuid")]
-#[cfg_attr(docsrs, doc(cfg(feature = "uuid")))]
-impl<S> SetRequestId<S, UuidRequestId> {
-    pub fn uuid(inner: S, header_name: HeaderName) -> Self {
-        Self::new(inner, UuidRequestId::new(header_name))
-    }
-}
-
-impl<S, M, ReqBody> Service<Request<ReqBody>> for SetRequestId<S, M>
+impl<S, M, ReqBody, ResBody> Service<Request<ReqBody>> for SetRequestId<S, M>
 where
-    S: Service<Request<ReqBody>>,
+    S: Service<Request<ReqBody>, Response = Response<ResBody>>,
     M: MakeRequestId,
 {
     type Response = S::Response;
@@ -146,27 +116,188 @@ where
         self.inner.poll_ready(cx)
     }
 
-    fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
-        let (mut parts, body) = req.into_parts();
-
-        match self.make_request_id.make_request_id(&parts) {
-            (header, Some(request_id)) => {
-                parts.extensions.insert(request_id.clone());
-                parts.headers.insert(header, request_id.0);
+    fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
+        if let Some(request_id) = req.headers().get(&self.header_name).cloned() {
+            if req.extensions().get::<RequestId>().is_none() {
+                req.extensions_mut().insert(RequestId::new(request_id));
             }
-            (header, None) => {
-                if parts.extensions.get::<RequestId>().is_none() {
-                    if let Some(request_id) = parts.headers.get(header) {
-                        parts.extensions.insert(RequestId::new(request_id.clone()));
-                    }
-                }
-            }
+        } else {
+            let request_id = self.make_request_id.make_request_id(&req);
+            req.extensions_mut().insert(request_id.clone());
+            req.headers_mut()
+                .insert(self.header_name.clone(), request_id.0);
         }
-
-        let req = Request::from_parts(parts, body);
 
         self.inner.call(req)
     }
 }
 
-// TODO(david): tests
+#[derive(Debug, Clone)]
+pub struct PropagateRequestIdLayer {
+    header_name: HeaderName,
+}
+
+impl PropagateRequestIdLayer {
+    pub fn new(header_name: HeaderName) -> Self {
+        PropagateRequestIdLayer { header_name }
+    }
+
+    pub fn x_request_id() -> Self {
+        Self::new(HeaderName::from_static(X_REQUEST_ID))
+    }
+}
+
+impl<S> Layer<S> for PropagateRequestIdLayer {
+    type Service = PropagateRequestId<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        PropagateRequestId::new(inner, self.header_name.clone())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PropagateRequestId<S> {
+    inner: S,
+    header_name: HeaderName,
+}
+
+impl<S> PropagateRequestId<S> {
+    pub fn new(inner: S, header_name: HeaderName) -> Self {
+        Self { inner, header_name }
+    }
+
+    pub fn x_request_id(inner: S) -> Self {
+        Self::new(inner, HeaderName::from_static(X_REQUEST_ID))
+    }
+
+    define_inner_service_accessors!();
+
+    pub fn layer(header_name: HeaderName) -> PropagateRequestIdLayer {
+        PropagateRequestIdLayer::new(header_name)
+    }
+}
+
+impl<S, ReqBody, ResBody> Service<Request<ReqBody>> for PropagateRequestId<S>
+where
+    S: Service<Request<ReqBody>, Response = Response<ResBody>>,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = PropagateRequestIdResponseFuture<S::Future>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
+        let request_id = req
+            .headers()
+            .get(&self.header_name)
+            .cloned()
+            .map(RequestId::new);
+
+        PropagateRequestIdResponseFuture {
+            inner: self.inner.call(req),
+            header_name: self.header_name.clone(),
+            request_id,
+        }
+    }
+}
+
+pin_project! {
+    /// Response future for [`PropagateRequestId`].
+    pub struct PropagateRequestIdResponseFuture<F> {
+        #[pin]
+        inner: F,
+        header_name: HeaderName,
+        request_id: Option<RequestId>,
+    }
+}
+
+impl<F, B, E> Future for PropagateRequestIdResponseFuture<F>
+where
+    F: Future<Output = Result<Response<B>, E>>,
+{
+    type Output = Result<Response<B>, E>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        let mut response = futures_core::ready!(this.inner.poll(cx))?;
+
+        if let Some(current_id) = response.headers().get(&*this.header_name) {
+            if response.extensions().get::<RequestId>().is_none() {
+                let current_id = current_id.clone();
+                response.extensions_mut().insert(current_id);
+            }
+        } else if let Some(request_id) = this.request_id.take() {
+            response
+                .headers_mut()
+                .insert(this.header_name.clone(), request_id.0.clone());
+            response.extensions_mut().insert(request_id);
+        }
+
+        Poll::Ready(Ok(response))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ServiceBuilderExt as _;
+    use hyper::{Body, Response};
+    use std::{
+        convert::Infallible,
+        sync::{
+            atomic::{AtomicU64, Ordering},
+            Arc,
+        },
+    };
+    use tower::{ServiceBuilder, ServiceExt};
+
+    #[allow(unused_imports)]
+    use super::*;
+
+    #[tokio::test]
+    async fn test_something() {
+        #[derive(Clone, Default)]
+        struct Counter(Arc<AtomicU64>);
+
+        impl MakeRequestId for Counter {
+            fn make_request_id<B>(&mut self, _request: &Request<B>) -> RequestId {
+                let id = HeaderValue::from_str(&self.0.fetch_add(1, Ordering::SeqCst).to_string())
+                    .unwrap();
+                RequestId::new(id)
+            }
+        }
+
+        let svc = ServiceBuilder::new()
+            .set_x_request_id(Counter::default())
+            .propagate_x_request_id()
+            .service_fn(handler);
+
+        // header on response
+        let req = Request::builder().body(Body::empty()).unwrap();
+        let res = svc.clone().oneshot(req).await.unwrap();
+        assert_eq!(res.headers()["x-request-id"], "0");
+
+        let req = Request::builder().body(Body::empty()).unwrap();
+        let res = svc.clone().oneshot(req).await.unwrap();
+        assert_eq!(res.headers()["x-request-id"], "1");
+
+        // doesn't override if header is already there
+        let req = Request::builder()
+            .header("x-request-id", "foo")
+            .body(Body::empty())
+            .unwrap();
+        let res = svc.clone().oneshot(req).await.unwrap();
+        assert_eq!(res.headers()["x-request-id"], "foo");
+
+        // extension propagated
+        let req = Request::builder().body(Body::empty()).unwrap();
+        let res = svc.clone().oneshot(req).await.unwrap();
+        assert_eq!(res.extensions().get::<RequestId>().unwrap().0, "2");
+    }
+
+    async fn handler(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+        Ok(Response::new(Body::empty()))
+    }
+}
