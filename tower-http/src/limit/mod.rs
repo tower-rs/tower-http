716848d5317a29e473bf9dfb4921d7239cb6d62e
Marcus Griep <marcus@griep.us>
2022-05-23 16:09:59 -0400

    docs: improve limit module documentation

--- tower-http/src/limit/mod.rs
+++ tower-http/src/limit/mod.rs
@@ -1,21 +1,21 @@
 //! Imposes a length limit on request bodies.
 //!
 //! This layer will also intercept requests with a `Content-Length` header
-//! larger than the allowable limit and return an immediate error before
-//! reading any of the body.
+//! larger than the allowable limit and return an immediate error response
+//! before reading any of the body.
 //!
-//! Note that payload length errors can be used by adversaries to attempt to
-//! smuggle requests. When an incoming stream is dropped due to an over-sized
-//! payload, servers should close the connection or resynchronize by
-//! optimistically consuming some data in an attempt to reach the end of the
-//! current HTTP frame. If the incoming stream cannot be resynchronized,
+//! Note that payload length errors can be used by adversaries in an attempt
+//! to smuggle requests. When an incoming stream is dropped due to an
+//! over-sized payload, servers should close the connection or resynchronize
+//! by optimistically consuming some data in an attempt to reach the end of
+//! the current HTTP frame. If the incoming stream cannot be resynchronized,
 //! then the connection should be closed.
 //!
 //! # Examples
 //!
-//! If the `Content-Length` header indicates a payload that is larger than
-//! the acceptable limit, then the response will be rejected whether or not
-//! the body is read.
+//! If a `Content-Length` header is present and indicates a payload that is
+//! larger than the acceptable limit, then the underlying service will not
+//! be called and a `413 Payload Too Large` response will be generated.
 //!
 //! ```rust
 //! use bytes::Bytes;
@@ -29,7 +29,7 @@
 //! # #[tokio::main]
 //! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
 //! async fn handle(req: Request<Limited<Body>>) -> Result<Response<Body>, Infallible> {
-//!     Ok(Response::new(Body::empty()))
+//!     panic!("This will not be hit")
 //! }
 //!
 //! let mut svc = ServiceBuilder::new()
@@ -53,13 +53,16 @@
 //! # }
 //! ```
 //!
-//! If no `Content-Length` header is present, then the body will be read
-//! until the length limit has been reached. If it is reached, the body
-//! will return an error. This error should be checked to determine if
-//! it is a
+//! If a `Content-Length` header is not present, then the body will be read
+//! until the configured limit has been reached. If the payload is larger than
+//! the limit, the [`http_body::Limited`] body will return an error. This
+//! error can be inspected to determine if is a [`http_body::LengthLimitError`]
+//! and return an appropriate response in such case.
 //!
-//! Note that if the body is never read, or never attempts to consume the
-//! body beyond the length limit, then no error will be generated.
+//! Note that no error will be generated if the body is never read. Similarly,
+//! if the body _would be_ to large, but is never consumed beyond the length
+//! limit, then no error is generated, and handling of the remaining incoming
+//! data stream is left to the server implementation as described above.
 //!
 //! ```rust
 //! # use bytes::Bytes;
@@ -111,9 +114,10 @@
 //! # }
 //! ```
 //!
-//! If the automatic `413 Payload Too Large` response and handling
-//! of `Content-Length` headers is not desired, consider directly using
-//! [`MapRequestBody`] to wrap the request body with [`http_body::Limited`].
+//! If enforcement of body size limits is desired without preemptively
+//! handling requests with a `Content-Length` header indicating an over-sized
+//! request, consider using [`MapRequestBody`] to wrap the request body with
+//! [`http_body::Limited`].
 //!
 //! [`MapRequestBody`]: crate::map_request_body
 //!
