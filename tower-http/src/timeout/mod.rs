28dea10de635bd50ddfff608b9d34577930a0d43
82marbag <69267416+82marbag@users.noreply.github.com>
2022-10-31 10:20:03 +0100

    Merge imports
    Signed-off-by: Daniele Ahmed <ahmeddan@amazon.de>

--- tower-http/src/timeout/mod.rs
+++ tower-http/src/timeout/mod.rs
@@ -3,11 +3,5 @@
 mod body;
 mod service;
 
-pub use body::TimeoutBody;
-pub use body::TimeoutError;
-pub use service::RequestBodyTimeout;
-pub use service::RequestBodyTimeoutLayer;
-pub use service::ResponseBodyTimeout;
-pub use service::ResponseBodyTimeoutLayer;
-pub use service::Timeout;
-pub use service::TimeoutLayer;
+pub use body::{TimeoutBody, TimeoutError};
+pub use service::{RequestBodyTimeout, RequestBodyTimeoutLayer, ResponseBodyTimeout, ResponseBodyTimeoutLayer, Timeout, TimeoutLayer};
