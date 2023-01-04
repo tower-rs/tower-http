c5188b8f72614dc5f2dcdfa8e88112d822cb88d3
Boris-Chengbiao Zhou <bobo1239@web.de>
2023-01-04 13:59:10 +0100

    Address review comment

--- tower-http/src/services/fs/serve_dir/future.rs
+++ tower-http/src/services/fs/serve_dir/future.rs
@@ -2,7 +2,7 @@ use super::{
     open_file::{FileOpened, FileRequestExtent, OpenFileOutput},
     DefaultServeDirFallback, ResponseBody,
 };
-use crate::{services::fs::AsyncReadBody, BoxError, content_encoding::Encoding};
+use crate::{content_encoding::Encoding, services::fs::AsyncReadBody, BoxError};
 use bytes::Bytes;
 use futures_util::{
     future::{BoxFuture, FutureExt, TryFutureExt},
@@ -214,11 +214,11 @@ fn build_response(output: FileOpened) -> Response<ResponseBody> {
         .header(header::CONTENT_TYPE, output.mime_header_value)
         .header(header::ACCEPT_RANGES, "bytes");
 
-    if let Some(encoding) = output.maybe_encoding {
-        // Identity encoding should not be included in the header
-        if encoding != Encoding::Identity {
-            builder = builder.header(header::CONTENT_ENCODING, encoding.into_header_value());
-        }
+    if let Some(encoding) = output
+        .maybe_encoding
+        .filter(|encoding| *encoding != Encoding::Identity)
+    {
+        builder = builder.header(header::CONTENT_ENCODING, encoding.into_header_value());
     }
 
     if let Some(last_modified) = output.last_modified {
