c967aa436f8dc8fc3ca3f27e9e1a9ea7efae16c2
Martin Bartlett <Martin Bartlett@dxc.com>
2023-10-20 12:30:10 +0200

    fix tests in propagate_extension

--- tower-http/src/propagate_extension.rs
+++ tower-http/src/propagate_extension.rs
@@ -182,12 +182,12 @@ mod tests {
 
 	use http::{Request, Response};
 	use std::convert::Infallible;
-	use tower::{Service, ServiceExt, ServiceBuilder, service_fn};
-	use tower_http::add_extension::AddExtensionLayer;
-	use tower_http::propagate_extension::PropagateExtensionLayer;
+	use tower::{Service, ServiceExt, ServiceBuilder};
+	use crate::add_extension::AddExtensionLayer;
+	//use tower_http::propagate_extension::PropagateExtensionLayer;
 	use hyper::Body;
 
-	async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+	async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
 		Ok(Response::new(Body::empty()))
 	}
 
