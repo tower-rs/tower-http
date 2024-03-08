5e87a5f1b4d6591d22271bbf402ff9ef693f81e2
Jack Lee <jack.y.l.dev@gmail.com>
2024-03-08 00:19:14 -0800

    add test

--- tower-http/src/cors/tests.rs
+++ tower-http/src/cors/tests.rs
@@ -4,7 +4,7 @@ use crate::test_helpers::Body;
 use http::{header, HeaderValue, Request, Response};
 use tower::{service_fn, util::ServiceExt, Layer};
 
-use crate::cors::CorsLayer;
+use crate::cors::{AllowOrigin, CorsLayer};
 
 #[tokio::test]
 #[allow(
@@ -30,4 +30,46 @@ async fn vary_set_by_inner_service() {
     assert_eq!(vary_headers.next(), Some(&CUSTOM_VARY_HEADERS));
     assert_eq!(vary_headers.next(), Some(&PERMISSIVE_CORS_VARY_HEADERS));
     assert_eq!(vary_headers.next(), None);
+}
+
+#[tokio::test]
+async fn test_allow_origin_async_predicate() {
+    #[derive(Clone)]
+    struct Client;
+
+    impl Client {
+        async fn fetch_allowed_origins(&self, _path: String) -> Vec<HeaderValue> {
+            vec![HeaderValue::from_static("http://example.com")]
+        }
+    }
+
+    let client = Client;
+
+    let allow_origin = AllowOrigin::async_predicate(move |origin, parts| {
+        let origin = origin.clone();
+        let client = client.clone();
+        let path = parts.uri.path().to_owned();
+
+        async move {
+            let origins = client.fetch_allowed_origins(path).await;
+
+            origins.contains(&origin)
+        }
+    });
+
+    let valid_origin = HeaderValue::from_static("http://example.com");
+    let parts = http::Request::new("hello world").into_parts().0;
+
+    let header = allow_origin
+        .to_header(Some(&valid_origin), &parts)
+        .await
+        .unwrap();
+    assert_eq!(header.0, header::ACCESS_CONTROL_ALLOW_ORIGIN);
+    assert_eq!(header.1, valid_origin);
+
+    let invalid_origin = HeaderValue::from_static("http://example.org");
+    let parts = http::Request::new("hello world").into_parts().0;
+
+    let res = allow_origin.to_header(Some(&invalid_origin), &parts).await;
+    assert!(res.is_none());
 }
