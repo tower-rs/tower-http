b3b8216e917c60ab66b4812b235faa406dc4718b
tottoto <tottotodev@gmail.com>
2025-08-21 22:41:10 +0900

    examples: Drop protoc dependency

--- examples/tonic-key-value-store/build.rs
+++ examples/tonic-key-value-store/build.rs
@@ -1,5 +1,4 @@
 fn main() {
-    tonic_prost_build::configure()
-        .compile_protos(&["key_value_store.proto"], &["proto"])
-        .unwrap();
+    let fds = protox::compile(["key_value_store.proto"], ["proto"]).unwrap();
+    tonic_prost_build::compile_fds(fds).unwrap();
 }
