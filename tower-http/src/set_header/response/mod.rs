7551a9b8b9706ca1e11c035659b243f688b136bd
Aminu 'Seun Joshua <34725212+seun-ja@users.noreply.github.com>
2026-05-08 23:55:52 +0100

    feat(set_header): refactor and improve multiple header middleware (#672)
    * feat(set_header): refactor and improve multiple header middleware
    
    Signed-off-by: Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>
    
    * fix docs test
    
    Signed-off-by: Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>
    
    * fix module use import on docs
    
    Signed-off-by: Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>
    
    * refactor multiple header middleware, improve docs, and update examples
    
    Signed-off-by: Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>
    
    * use publicly available trait instead
    
    Signed-off-by: Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>
    
    ---------
    
    Signed-off-by: Aminu Oluwaseun Joshua <seun.aminujoshua@gmail.com>

---
+++ tower-http/src/set_header/response/mod.rs
@@ -0,0 +1,173 @@
+//! Middleware for setting headers on HTTP responses.
+//!
+//! This module provides middleware for setting one or more headers on HTTP responses, either with fixed values or values determined dynamically from the response.
+//!
+//! # Single Header
+//!
+//! Use [`SetResponseHeaderLayer`] and [`SetResponseHeader`] to set a single header. The header value can be a fixed value or computed dynamically using a closure. See [`crate::set_header::MakeHeaderValue`] for details.
+//!
+//! ## Example: Fixed Value
+//!
+//! ```
+//! use http::{Request, Response, header::{self, HeaderValue}};
+//! use tower::{Service, ServiceExt, ServiceBuilder};
+//! use tower_http::set_header::SetResponseHeaderLayer;
+//! use http_body_util::Full;
+//! use bytes::Bytes;
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! # let render_html = tower::service_fn(|request: Request<Full<Bytes>>| async move {
+//! #     Ok::<_, std::convert::Infallible>(Response::new(request.into_body()))
+//! # });
+//!
+//! let mut svc = ServiceBuilder::new()
+//!     .layer(
+//!         SetResponseHeaderLayer::if_not_present(
+//!             header::CONTENT_TYPE,
+//!             HeaderValue::from_static("text/html"),
+//!         )
+//!     )
+//!     .service(render_html);
+//!
+//! let request = Request::new(Full::default());
+//!
+//! let response = svc.ready().await?.call(request).await?;
+//!
+//! assert_eq!(response.headers()["content-type"], "text/html");
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ## Example: Dynamic Value
+//!
+//! ```
+//! use http::{Request, Response, header::{self, HeaderValue}};
+//! use tower::{Service, ServiceExt, ServiceBuilder};
+//! use tower_http::set_header::SetResponseHeaderLayer;
+//! use bytes::Bytes;
+//! use http_body_util::Full;
+//! use http_body::Body as _; // for `Body::size_hint`
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! # let render_html = tower::service_fn(|request: Request<Full<Bytes>>| async move {
+//! #     Ok::<_, std::convert::Infallible>(Response::new(Full::from("1234567890")))
+//! # });
+//!
+//! let mut svc = ServiceBuilder::new()
+//!     .layer(
+//!         SetResponseHeaderLayer::overriding(
+//!             header::CONTENT_LENGTH,
+//!             |response: &Response<Full<Bytes>>| {
+//!                 if let Some(size) = response.body().size_hint().exact() {
+//!                     Some(HeaderValue::from_str(&size.to_string()).unwrap())
+//!                 } else {
+//!                     None
+//!                 }
+//!             }
+//!         )
+//!     )
+//!     .service(render_html);
+//!
+//! let request = Request::new(Full::default());
+//!
+//! let response = svc.ready().await?.call(request).await?;
+//!
+//! assert_eq!(response.headers()["content-length"], "10");
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! # Multiple Headers
+//!
+//! Use [`SetMultipleResponseHeadersLayer`] and [`SetMultipleResponseHeader`] to set multiple headers at once. Each header can have a fixed value or be computed dynamically.
+//!
+//! ## Example: Multiple Fixed Values
+//!
+//! ```
+//! use http::{Request, Response, header::{self, HeaderValue}};
+//! use tower::{Service, ServiceExt, ServiceBuilder};
+//! use tower_http::set_header::response::{SetMultipleResponseHeadersLayer, HeaderMetadata};
+//! use http_body_util::Full;
+//! use bytes::Bytes;
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! # let render_html = tower::service_fn(|request: Request<Full<Bytes>>| async move {
+//! #     Ok::<_, std::convert::Infallible>(Response::new(request.into_body()))
+//! # });
+//!
+//! let mut svc = ServiceBuilder::new()
+//!     .layer(
+//!         SetMultipleResponseHeadersLayer::overriding(vec![
+//!             (header::CONTENT_TYPE, HeaderValue::from_static("text/html")).into(),
+//!             (header::CACHE_CONTROL, HeaderValue::from_static("no-cache")).into(),
+//!         ])
+//!     )
+//!     .service(render_html);
+//!
+//! let request = Request::new(Full::default());
+//!
+//! let response = svc.ready().await?.call(request).await?;
+//!
+//! assert_eq!(response.headers()["content-type"], "text/html");
+//! assert_eq!(response.headers()["cache-control"], "no-cache");
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ## Example: Multiple Dynamic Values
+//!
+//! ```
+//! use http::{Request, Response, header::{self, HeaderValue}};
+//! use tower::{Service, ServiceExt, ServiceBuilder};
+//! use tower_http::set_header::response::{SetMultipleResponseHeadersLayer, HeaderMetadata};
+//! use bytes::Bytes;
+//! use http_body_util::Full;
+//! use http_body::Body as _; // for `Body::size_hint`
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! # let render_html = tower::service_fn(|request: Request<Full<Bytes>>| async move {
+//! #     Ok::<_, std::convert::Infallible>(Response::new(Full::from("1234567890")))
+//! # });
+//!
+//! let mut svc = ServiceBuilder::new()
+//!     .layer(
+//!         SetMultipleResponseHeadersLayer::overriding(vec![
+//!             (header::CONTENT_LENGTH, |response: &Response<Full<Bytes>>| {
+//!                 if let Some(size) = response.body().size_hint().exact() {
+//!                     Some(HeaderValue::from_str(&size.to_string()).unwrap())
+//!                 } else {
+//!                     None
+//!                 }
+//!             }).into(),
+//!         ])
+//!     )
+//!     .service(render_html);
+//!
+//! let request = Request::new(Full::default());
+//!
+//! let response = svc.ready().await?.call(request).await?;
+//!
+//! assert_eq!(response.headers()["content-length"], "10");
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! # Modes
+//!
+//! - `overriding`: If a previous value exists for the same header, it is removed and replaced with the new value.
+//! - `appending`: The new header is always added, preserving any existing values. If previous values exist, the header will have multiple values.
+//! - `if_not_present`: If a previous value exists for the header, the new value is not inserted.
+//!
+//! See [`SetResponseHeaderLayer`], [`SetResponseHeader`], [`SetMultipleResponseHeadersLayer`], and [`SetMultipleResponseHeader`] for more details.
+
+mod multiple_header;
+mod single_header;
+
+pub use multiple_header::{
+    HeaderMetadata, SetMultipleResponseHeader, SetMultipleResponseHeadersLayer,
+};
+pub use single_header::{SetResponseHeader, SetResponseHeaderLayer};
---
+++ tower-http/src/set_header/response/multiple_header.rs
@@ -0,0 +1,533 @@
+//! Set multiple headers on the response.
+//!
+//! See the root [`crate::set_header::response`] module for full documentation and usage examples.
+//!
+use http::{header::HeaderName, HeaderValue, Request, Response};
+use pin_project_lite::pin_project;
+use std::{
+    fmt,
+    future::Future,
+    pin::Pin,
+    task::{ready, Context, Poll},
+};
+use tower_layer::Layer;
+use tower_service::Service;
+
+use crate::set_header::{InsertHeaderMode, MakeHeaderValue};
+
+/// A trait that combines MakeHeaderValue and Clone capability for trait objects.
+trait CloneableMakeHeaderValue<T>: MakeHeaderValue<T> + Send + Sync {
+    fn clone_box(&self) -> Box<dyn CloneableMakeHeaderValue<T>>;
+}
+
+impl<T, M> CloneableMakeHeaderValue<T> for M
+where
+    M: MakeHeaderValue<T> + Clone + Send + Sync + 'static,
+{
+    fn clone_box(&self) -> Box<dyn CloneableMakeHeaderValue<T>> {
+        Box::new(self.clone())
+    }
+}
+
+/// A "Bridge" struct that allows for trait object-based header value generation.
+struct BoxedMakeHeaderValue<T>(Box<dyn CloneableMakeHeaderValue<T>>);
+
+impl<T> BoxedMakeHeaderValue<T> {
+    /// Create a new BoxedMakeHeaderValue from any maker that implements MakeHeaderValue and Clone.
+    fn new<M>(maker: M) -> Self
+    where
+        M: MakeHeaderValue<T> + Clone + Send + Sync + 'static,
+    {
+        Self(Box::new(maker))
+    }
+}
+
+impl<T> Clone for BoxedMakeHeaderValue<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone_box())
+    }
+}
+
+impl<T> MakeHeaderValue<T> for BoxedMakeHeaderValue<T> {
+    fn make_header_value(&mut self, message: &T) -> Option<HeaderValue> {
+        self.0.make_header_value(message)
+    }
+}
+
+impl<T> fmt::Debug for BoxedMakeHeaderValue<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("BoxedMakeHeaderValue").finish()
+    }
+}
+
+/// Metadata describing a response header to be set by [`SetMultipleResponseHeadersLayer`] or [`SetMultipleResponseHeader`].
+#[derive(Clone, Debug)]
+pub struct HeaderMetadata<T> {
+    /// The name of the header to set.
+    header_name: HeaderName,
+    /// The value or value factory for the header.
+    make: BoxedMakeHeaderValue<T>,
+}
+
+impl<T> HeaderMetadata<T> {
+    /// Create a new HeaderMetadata with the given header name and value factory.
+    fn new<M: MakeHeaderValue<T> + Clone + 'static + Send + Sync>(
+        header_name: HeaderName,
+        make: M,
+    ) -> Self {
+        Self {
+            header_name,
+            make: BoxedMakeHeaderValue::new(make),
+        }
+    }
+
+    /// Convert this metadata into a [`HeaderInsertionConfig`] with the given insertion mode.
+    fn build_config(self, mode: InsertHeaderMode) -> HeaderInsertionConfig<T> {
+        HeaderInsertionConfig {
+            header_name: self.header_name,
+            make: self.make,
+            mode,
+        }
+    }
+}
+
+impl<T, M> From<(HeaderName, M)> for HeaderMetadata<T>
+where
+    M: MakeHeaderValue<T> + Clone + 'static + Send + Sync,
+{
+    fn from((header_name, make): (HeaderName, M)) -> Self {
+        HeaderMetadata::new(header_name, make)
+    }
+}
+
+struct HeaderInsertionConfig<T> {
+    header_name: HeaderName,
+    make: BoxedMakeHeaderValue<T>,
+    mode: InsertHeaderMode,
+}
+
+impl<T> Clone for HeaderInsertionConfig<T>
+where
+    BoxedMakeHeaderValue<T>: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            header_name: self.header_name.clone(),
+            make: self.make.clone(),
+            mode: self.mode,
+        }
+    }
+}
+
+impl<T> fmt::Debug for HeaderInsertionConfig<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("HeaderInsertionConfig")
+            .field("header_name", &self.header_name)
+            .field("mode", &self.mode)
+            .field("make", &"BoxedMakeHeaderValue")
+            .finish()
+    }
+}
+
+/// Layer that applies [`SetMultipleResponseHeader`] which adds multiple response headers.
+///
+/// See [`SetMultipleResponseHeader`] for more details.
+pub struct SetMultipleResponseHeadersLayer<T> {
+    headers: Vec<HeaderInsertionConfig<T>>,
+}
+
+impl<T> fmt::Debug for SetMultipleResponseHeadersLayer<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SetMultipleResponseHeadersLayer")
+            .field("headers", &self.headers)
+            .finish()
+    }
+}
+
+impl<T> SetMultipleResponseHeadersLayer<T> {
+    /// Create a new [`SetMultipleResponseHeadersLayer`] that overrides any existing values for the same header.
+    ///
+    /// If any previous value exists for the same header, it is removed and replaced with the new matching header value.
+    pub fn overriding(metadata: Vec<HeaderMetadata<T>>) -> Self {
+        let headers: Vec<HeaderInsertionConfig<T>> = metadata
+            .into_iter()
+            .map(|m| m.build_config(InsertHeaderMode::Override))
+            .collect();
+
+        Self::new(headers)
+    }
+
+    /// Create a new [`SetMultipleResponseHeadersLayer`] that appends header values.
+    ///
+    /// The new header is always added, preserving any existing values. If previous values exist, the header will have multiple values.
+    pub fn appending(metadata: Vec<HeaderMetadata<T>>) -> Self {
+        let headers: Vec<HeaderInsertionConfig<T>> = metadata
+            .into_iter()
+            .map(|m| m.build_config(InsertHeaderMode::Append))
+            .collect();
+
+        Self::new(headers)
+    }
+
+    /// Create a new [`SetMultipleResponseHeadersLayer`] that only inserts if the header is not already present.
+    ///
+    /// If a previous value exists for the header, the new value is not inserted.
+    pub fn if_not_present(metadata: Vec<HeaderMetadata<T>>) -> Self {
+        let headers: Vec<HeaderInsertionConfig<T>> = metadata
+            .into_iter()
+            .map(|m| m.build_config(InsertHeaderMode::IfNotPresent))
+            .collect();
+
+        Self::new(headers)
+    }
+
+    /// Internal constructor for a new [`SetMultipleResponseHeadersLayer`] from a list of headers.
+    fn new(headers: Vec<HeaderInsertionConfig<T>>) -> Self {
+        Self { headers }
+    }
+}
+
+impl<S, T> Layer<S> for SetMultipleResponseHeadersLayer<T> {
+    type Service = SetMultipleResponseHeader<S, T>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        SetMultipleResponseHeader {
+            inner,
+            headers: self.headers.clone(),
+        }
+    }
+}
+
+/// Middleware that sets multiple headers on the response.
+
+#[derive(Clone)]
+pub struct SetMultipleResponseHeader<S, T> {
+    inner: S,
+    headers: Vec<HeaderInsertionConfig<T>>,
+}
+
+impl<S, T> SetMultipleResponseHeader<S, T> {
+    /// Create a new [`SetMultipleResponseHeader`] that overrides any existing values for the same header.
+    ///
+    /// If a previous value exists for the same header, it is removed and replaced with the new header value.
+    pub fn overriding(inner: S, metadata: Vec<HeaderMetadata<T>>) -> Self {
+        let headers: Vec<HeaderInsertionConfig<T>> = metadata
+            .into_iter()
+            .map(|m| m.build_config(InsertHeaderMode::Override))
+            .collect();
+
+        Self::new(inner, headers)
+    }
+
+    /// Create a new [`SetMultipleResponseHeader`] that appends header values.
+    ///
+    /// The new header is always added, preserving any existing values. If previous values exist, the header will have multiple values.
+    pub fn appending(inner: S, metadata: Vec<HeaderMetadata<T>>) -> Self {
+        let headers: Vec<HeaderInsertionConfig<T>> = metadata
+            .into_iter()
+            .map(|m| m.build_config(InsertHeaderMode::Append))
+            .collect();
+
+        Self::new(inner, headers)
+    }
+
+    /// Create a new [`SetMultipleResponseHeader`] that only inserts if the header is not already present.
+    ///
+    /// If a previous value exists for the header, the new value is not inserted.
+    pub fn if_not_present(inner: S, metadata: Vec<HeaderMetadata<T>>) -> Self {
+        let headers: Vec<HeaderInsertionConfig<T>> = metadata
+            .into_iter()
+            .map(|m| m.build_config(InsertHeaderMode::IfNotPresent))
+            .collect();
+
+        Self::new(inner, headers)
+    }
+
+    /// Internal constructor for a new [`SetMultipleResponseHeader`] from an inner service and a list of headers.
+    fn new(inner: S, headers: Vec<HeaderInsertionConfig<T>>) -> Self {
+        Self { inner, headers }
+    }
+
+    define_inner_service_accessors!();
+}
+
+impl<S, T> fmt::Debug for SetMultipleResponseHeader<S, T>
+where
+    S: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SetMultipleResponseHeader")
+            .field("inner", &self.inner)
+            .field("headers", &self.headers)
+            .finish()
+    }
+}
+
+impl<ReqBody, ResBody, S> Service<Request<ReqBody>>
+    for SetMultipleResponseHeader<S, Response<ResBody>>
+where
+    S: Service<Request<ReqBody>, Response = Response<ResBody>>,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = ResponseFuture<S::Future, Response<ResBody>>;
+
+    #[inline]
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    /// Call the inner service and apply all configured headers to the response.
+    fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
+        ResponseFuture {
+            future: self.inner.call(req),
+            headers: self.headers.clone(),
+        }
+    }
+}
+
+pin_project! {
+    /// Response future for [`SetMultipleResponseHeader`].
+    #[derive(Debug)]
+    pub struct ResponseFuture<F, T> {
+        #[pin]
+        future: F,
+        headers: Vec<HeaderInsertionConfig<T>>,
+    }
+}
+
+impl<F, ResBody, E> Future for ResponseFuture<F, Response<ResBody>>
+where
+    F: Future<Output = Result<Response<ResBody>, E>>,
+{
+    type Output = F::Output;
+
+    /// Polls the inner future and applies all configured headers to the response before returning it.
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        let mut res = ready!(this.future.poll(cx)?);
+
+        for header in this.headers {
+            header
+                .mode
+                .apply(&header.header_name, &mut res, &mut header.make);
+        }
+
+        Poll::Ready(Ok(res))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_helpers::Body;
+    use http::{header, HeaderValue};
+    use std::convert::Infallible;
+    use tower::{service_fn, ServiceExt};
+
+    #[tokio::test]
+    async fn test_override_mode() {
+        let svc = SetMultipleResponseHeader::overriding(
+            service_fn(|_req: Request<Body>| async {
+                let res = Response::builder()
+                    .header(header::CONTENT_TYPE, "good-content")
+                    .body(Body::empty())
+                    .unwrap();
+                Ok::<_, Infallible>(res)
+            }),
+            vec![(header::CONTENT_TYPE, HeaderValue::from_static("text/html")).into()],
+        );
+
+        let res = svc.oneshot(Request::new(Body::empty())).await.unwrap();
+
+        let mut values = res.headers().get_all(header::CONTENT_TYPE).iter();
+        assert_eq!(values.next().unwrap(), "text/html");
+        assert_eq!(values.next(), None);
+    }
+
+    #[tokio::test]
+    async fn test_append_mode() {
+        let svc = SetMultipleResponseHeader::appending(
+            service_fn(|_req: Request<Body>| async {
+                let res = Response::builder()
+                    .header(header::CONTENT_TYPE, "good-content")
+                    .body(Body::empty())
+                    .unwrap();
+                Ok::<_, Infallible>(res)
+            }),
+            vec![(header::CONTENT_TYPE, HeaderValue::from_static("text/html")).into()],
+        );
+
+        let res = svc.oneshot(Request::new(Body::empty())).await.unwrap();
+
+        let mut values = res.headers().get_all(header::CONTENT_TYPE).iter();
+        assert_eq!(values.next().unwrap(), "good-content");
+        assert_eq!(values.next().unwrap(), "text/html");
+        assert_eq!(values.next(), None);
+    }
+
+    #[tokio::test]
+    async fn test_skip_if_present_mode() {
+        let svc = SetMultipleResponseHeader::if_not_present(
+            service_fn(|_req: Request<Body>| async {
+                let res = Response::builder()
+                    .header(header::CONTENT_TYPE, "good-content")
+                    .body(Body::empty())
+                    .unwrap();
+                Ok::<_, Infallible>(res)
+            }),
+            vec![(header::CONTENT_TYPE, HeaderValue::from_static("text/html")).into()],
+        );
+
+        let res = svc.oneshot(Request::new(Body::empty())).await.unwrap();
+
+        let mut values = res.headers().get_all(header::CONTENT_TYPE).iter();
+        assert_eq!(values.next().unwrap(), "good-content");
+        assert_eq!(values.next(), None);
+    }
+
+    #[tokio::test]
+    async fn test_skip_if_present_mode_when_not_present() {
+        let svc = SetMultipleResponseHeader::if_not_present(
+            service_fn(|_req: Request<Body>| async {
+                let res = Response::builder().body(Body::empty()).unwrap();
+                Ok::<_, Infallible>(res)
+            }),
+            vec![(header::CONTENT_TYPE, HeaderValue::from_static("text/html")).into()],
+        );
+
+        let res = svc.oneshot(Request::new(Body::empty())).await.unwrap();
+
+        let mut values = res.headers().get_all(header::CONTENT_TYPE).iter();
+        assert_eq!(values.next().unwrap(), "text/html");
+        assert_eq!(values.next(), None);
+    }
+
+    #[test]
+    fn test_tuple_metadata_impl() {
+        let tuple: (HeaderName, HeaderValue) =
+            (header::CONTENT_TYPE, HeaderValue::from_static("foo"));
+        let meta: HeaderMetadata<HeaderValue> = tuple.into();
+        assert_eq!(meta.header_name, header::CONTENT_TYPE);
+        // Check that the header value is correct by making a header value from meta.make
+        let mut make = meta.make.clone();
+        assert_eq!(
+            make.make_header_value(&HeaderValue::from_static("foo")),
+            Some(HeaderValue::from_static("foo"))
+        );
+    }
+
+    #[test]
+    fn test_convert_to_header_config_struct_and_tuple() {
+        let meta: HeaderMetadata<HeaderValue> = HeaderMetadata::<HeaderValue> {
+            header_name: header::CONTENT_TYPE,
+            make: BoxedMakeHeaderValue::new(HeaderValue::from_static("bar")),
+        };
+        let rh = meta.build_config(crate::set_header::InsertHeaderMode::Override);
+        assert_eq!(rh.header_name, header::CONTENT_TYPE);
+        let mut make = rh.make.clone();
+        assert_eq!(
+            make.make_header_value(&HeaderValue::from_static("bar")),
+            Some(HeaderValue::from_static("bar"))
+        );
+
+        let tuple: (HeaderName, HeaderValue) =
+            (header::CONTENT_TYPE, HeaderValue::from_static("baz"));
+        let meta: HeaderMetadata<HeaderValue> = tuple.into();
+        let rh2 = meta.build_config(crate::set_header::InsertHeaderMode::Override);
+        assert_eq!(rh2.header_name, header::CONTENT_TYPE);
+        let mut make2 = rh2.make.clone();
+        assert_eq!(
+            make2.make_header_value(&HeaderValue::from_static("baz")),
+            Some(HeaderValue::from_static("baz"))
+        );
+    }
+
+    #[test]
+    fn test_debug_impls() {
+        let meta: HeaderMetadata<HeaderValue> =
+            (header::CONTENT_TYPE, HeaderValue::from_static("bar")).into();
+        let rh = meta
+            .clone()
+            .build_config(crate::set_header::InsertHeaderMode::Override);
+        let layer = SetMultipleResponseHeadersLayer::overriding(vec![meta]);
+        let debug_str = format!("{:?}", layer);
+        assert!(debug_str.contains("SetMultipleResponseHeadersLayer"));
+        let debug_rh = format!("{:?}", rh);
+        assert!(debug_rh.contains("HeaderInsertionConfig"));
+
+        let svc = SetMultipleResponseHeader::overriding(
+            tower::service_fn(|_req: Request<Body>| async {
+                Ok::<_, std::convert::Infallible>(Response::new(Body::empty()))
+            }),
+            vec![(header::CONTENT_TYPE, HeaderValue::from_static("foo")).into()]
+                as Vec<HeaderMetadata<HeaderValue>>,
+        );
+        let debug_svc = format!("{:?}", svc);
+        assert!(debug_svc.contains("SetMultipleResponseHeader"));
+    }
+
+    #[tokio::test]
+    async fn test_layer_construction_and_multiple_headers() {
+        // Multiple different headers in the same vec
+        let svc = tower::ServiceBuilder::new()
+            .layer(SetMultipleResponseHeadersLayer::overriding(vec![
+                (header::CONTENT_TYPE, HeaderValue::from_static("text/html")).into(),
+                (header::CACHE_CONTROL, HeaderValue::from_static("no-cache")).into(),
+            ]))
+            .service(service_fn(|_req: Request<Body>| async {
+                Ok::<_, Infallible>(Response::new(Body::empty()))
+            }));
+
+        let res = svc.oneshot(Request::new(Body::empty())).await.unwrap();
+        assert_eq!(res.headers()["content-type"], "text/html");
+        assert_eq!(res.headers()["cache-control"], "no-cache");
+    }
+
+    #[tokio::test]
+    async fn test_layer_with_empty_vec() {
+        let svc = tower::ServiceBuilder::new()
+            .layer(SetMultipleResponseHeadersLayer::<Response<Body>>::overriding(vec![]))
+            .service(service_fn(|_req: Request<Body>| async {
+                Ok::<_, Infallible>(Response::new(Body::empty()))
+            }));
+
+        let res = svc.oneshot(Request::new(Body::empty())).await.unwrap();
+        // No headers should be set
+        assert_eq!(res.headers().len(), 0);
+    }
+
+    #[tokio::test]
+    async fn test_layer_with_static_and_closure_headers_fixed() {
+        // Wrap the static value
+        let static_meta = (header::CONTENT_TYPE, HeaderValue::from_static("text/html")).into();
+
+        // Wrap the closure
+        let closure_meta = (header::X_FRAME_OPTIONS, |_res: &Response<Body>| {
+            Some(HeaderValue::from_static("DENY"))
+        })
+            .into();
+
+        let svc = tower::ServiceBuilder::new()
+            .layer(SetMultipleResponseHeadersLayer::overriding(vec![
+                static_meta,
+                closure_meta,
+            ]))
+            .service(service_fn(|_req: Request<Body>| async {
+                Ok::<_, Infallible>(Response::new(Body::empty()))
+            }));
+
+        let res = svc.oneshot(Request::new(Body::empty())).await.unwrap();
+        assert_eq!(res.headers()["content-type"], "text/html");
+        assert_eq!(res.headers()["x-frame-options"], "DENY");
+    }
+
+    #[test]
+    fn test_debug_layer_and_service() {
+        let meta: HeaderMetadata<HeaderValue> =
+            (header::CONTENT_TYPE, HeaderValue::from_static("foo")).into();
+        let layer = SetMultipleResponseHeadersLayer::overriding(vec![meta]);
+        let debug_str = format!("{:?}", layer);
+        assert!(debug_str.contains("SetMultipleResponseHeadersLayer"));
+    }
+}
